a8b78768be47782bbb6705396e4cbe48f410d951
hanolee <hano.dev.kr@gmail.com>
2020-12-22 07:55:27 +0900

    Props를 배웠다. Props는 만들어진 객체에 태그안에 값을 선언해서 그가 선언된 위치로 값을 넘겨주는 역할을 하였다.
    <Person/>를 쓸수도 있고, <Person></Person>을 쓸 수 도 있는데, <></>형태안에 값을 넣고 싶다면 선언부에서 props.children이 존재해야한다.
    <Person name = "hano"/>의 형태에서는 name -> 선언부 방향이지만,
    
    <Person>hihihi</Person>에서는 선언부의 props.children -> hihihi 느낌 ㅋㅌㅋ 정확한건 잘 모름.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -10,9 +10,9 @@ class App extends React.Component{
       <div className = "App">
         <h1>title</h1>
         <p>this is really working!</p>
-        <Person/>
-        <Person/>
-        <Person/>
+        <Person name = "Hano" age = "24"/>
+        <Person name = "Mina" age = "23">My hobbies : Racing</Person>
+        <Person name = "Jieun" age = "26"/>
       </div>
     )
   }
