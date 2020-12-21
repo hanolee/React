ecb845149e8a8a7e951bb27fa8727829e648a9dc
hanolee <hano.dev.kr@gmail.com>
2020-12-22 08:16:20 +0900

    버튼을 작동하기 위해서는 그냥 클릭하면 된다. 버튼을 작동하여 어떤 동작을 수행하게 하고 싶으면 onClick = {}을 <>안에 추가해주면 되는데 이때 {}안에 들어가는 것을 eventhandler라고 하는데, 내가 아직까지 이해한바로는 이 이벤트핸들러가 별다른게 아니고, 함수임ㅋㅋㅋㅋ 함수는 const handler = () => {} 요렇게 선언하는게 대세인듯. 함수라고 해서 {}안에 handler() 이렇게 넣으면 안되고, this.handler 까지만 넣어준다. reference 정도만 넘겨주는 것이기 때문.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,22 +7,28 @@ class App extends React.Component{
 
   state = {
     persons : [
-      {name : 'Max', age : 28}
-      {name : 'Manu', age : 23}
+      {name : 'Max', age : 28},
+      {name : 'Manu', age : 23},
       {name : 'Stephanie', age : 28}
     ]
     
   }
+
+  switchNameHandler = () => {
+    console.log('Was Clicked');
+    
+  }
+
 
   render(){
     return (
       <div className = "App">
         <h1>title</h1>
         <p>this is really working!</p>
-        <button>Switch Name</button>
+        <button onClick = {this.switchNameHandler}>Switch Name</button>
         <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
-        <Person name = {this.state.persons[1].name} age = {this.state.persons[0].age}>My hobbies : Racing</Person>
-        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My hobbies : Racing</Person>
+        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
       </div>
     )
   }
