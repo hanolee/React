721f84e0c0b96aa220be05826daadbf948b35ab7
hanolee <hano.dev.kr@gmail.com>
2020-12-22 09:02:28 +0900

    함수안에 함수를 선언할 수 있다. useState를 잘 써야 된다. react hook이 useState 클래스형 컴포넌트에서는 setState를 하게되면 변화한 state만 바꿔주어 merge하는데, 함수형 컴포넌트에서는 [name, setName]관계에서 setName을 해주면 merge가 아니라 replace 하지만 다른 곳에서 [age, setAge]이런식으로 선언되어 있으면 그 값은 바뀌지 않음.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,44 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { createElement } from 'react'
+import React, {useState} from 'react'
 import Person from './Person/Person';
 
-class App extends React.Component{
+const App = (props) => {
 
-  state = {
+  const [personsState, setPersonsState] = useState({
     persons : [
       {name : 'Max', age : 28},
       {name : 'Manu', age : 23},
       {name : 'Stephanie', age : 28}
-    ],
-    otherState : 'some other value'
-    
-  }
+    ]
+  });
+const [otherState, setOtherState] = useState('some other value');
 
-  switchNameHandler = () => {
+console.log(personsState, otherState)
+
+  const switchNameHandler = () => {
     //console.log('Was Clicked');
     //Don't to this - this.state.persons[0].name = 'hano';
-    this.setState({persons: [
+    setPersonsState({persons: [
       {name : 'Hano', age : 24},
       {name : 'Jieun', age : 26},
       {name : 'Hanoo', age : 22}
-    ]
+    ],
+    otherState : personsState.otherState
   })
   }
 
 
-  render(){
+  
     return (
       <div className = "App">
         <h1>title</h1>
         <p>this is really working!</p>
-        <button onClick = {this.switchNameHandler}>Switch Name</button>
-        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
-        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My hobbies : Racing</Person>
-        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
+        <button onClick = {switchNameHandler}>Switch Name</button>
+        <Person 
+        name = {personsState.persons[0].name} 
+        age = {personsState.persons[0].age}/>
+        <Person 
+        name = {personsState.persons[1].name} 
+        age = {personsState.persons[1].age}>My hobbies : Racing</Person>
+        <Person 
+        name = {personsState.persons[2].name} 
+        age = {personsState.persons[2].age}/>
       </div>
-    )
-  }
+    );
+  
 
 }
 
