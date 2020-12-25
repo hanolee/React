f9e0f651b4a4486ee331733316783cdd4a68bb73
hanolee <hano.dev.kr@gmail.com>
2020-12-26 05:21:33 +0900

    Ternary => Javascript experssion

--- react-complet-guide-class/src/App.js
+++ react-complet-guide-class/src/App.js
@@ -53,17 +53,11 @@ class App extends Component {
       cursor: 'pointer'
 
     };
+    let persons = null;
 
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}>Switch Name</button>
-        {this.state.showPersons ?
+    if (this.state.showPersons) {
+      persons = (
         <div>
-
           <Person
             name={this.state.persons[0].name}
             age={this.state.persons[0].age}
@@ -80,8 +74,18 @@ class App extends Component {
             name={this.state.persons[2].name}
             age={this.state.persons[2].age}
             />
-        </div> : null
-  }
+        </div> 
+      )
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
