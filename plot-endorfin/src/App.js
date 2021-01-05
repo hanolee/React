44d12f3a94e43b1d60c168eaa37eeff6a832c606
hanolee <hano.dev.kr@gmail.com>
2021-01-05 10:01:17 +0900

    props, state - plotly

--- plot-endorfin/src/App.js
+++ plot-endorfin/src/App.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React , {useState} from 'react'
 import EndorfinChart from './Components/endorfinChart'
+//import './Contract/oraclecontracts_Mainnet'
 
 function App() {
+  
+  const [index, setIndex] =  useState(0)
+  
+  let a = 12
+  let b = 34
+  let c = 40
+
+  const sum = ()=>{
+    setIndex(index + a+b+c)
+  }
+  const push = () =>{
+    
+  }
+  //블록스탬프 찍어서 x값 설정 86400으로 나눠서 1440으로 나누고 등등 시간을 추가해주기
+  //y값은 sum함수 이용해서 3개의 값 더해주고, 배열에 push해주기
+  const data = [
+    {
+      x: [1, 2, 3],
+      y: [2, 6, 3],
+      type: 'scatter',
+      mode: 'lines+markers',
+      marker: {color: 'red'},
+    },
+    {type: 'line', x: [1, 2, 3], y: [2, 5, 3]},
+  ]
+const layout = {width: 520, height: 340, title: 'A Fancy Plot'}
+
   return (
     <div className="App">
+      <h1>현재 가격은 {index}입니다</h1>
+      <button onClick = {sum}>a,b,c 더하기</button>
       
+      <h1>그래프</h1>
+      <EndorfinChart data = {data} layout = {layout} isFulled = {true}/>
+      <EndorfinChart data = {data} layout = {layout} isFulled = {false}/>
+      <EndorfinChart data = {data} layout = {layout} isFulled = {true}/>
     </div>
   );
 }
--- plot-endorfin/src/Components/endorfinChart.js
+++ plot-endorfin/src/Components/endorfinChart.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Plot from 'react-plotly.js';
 
 const endorfinChart = (props) => {
     
     return(
         <div>
+            {props.isFulled} ? <div>
             <Plot
                 data={props.data}
                 layout={props.layout}/>
+            
+
+
             <button>Reset</button>
+            </div>
+            : 
+            <div>없어요</div>
         </div>
     )
 }
